7912f7939c44f4206f8cb1d415ee5dc92db036d3
AlbaZoydo <alba.zoydo@edu.uah.es>
2026-02-05 11:17:33 +0100

    a

--- Generador.py
+++ Generador.py
@@ -2,7 +2,7 @@ import random
 import csv
 
 with open("Datos_tabla.csv", "w", encoding="utf-8") as f:
-    for i in range(1, 30000000 + 1):
+    for i in range(0, 30000000):
         nombre = f"Estudiante_{i}"
         codigo_carrera = random.randint(0, 100)
         edad = random.randint(18, 40)
