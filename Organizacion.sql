6b55f96d1671524788a1f64acb58c76574aec6dd
Bea-Wick <beatriz.arribass@edu.uah.es>
2026-02-06 17:00:15 +0100

    Correccion tabla estudiantes 2

--- Organizacion.sql
+++ Organizacion.sql
@@ -13,22 +13,23 @@ CREATE TABLE IF NOT EXISTS pl1.estudiantes(
 \echo 'Cargando'
 \COPY pl1.estudiantes    FROM './Datos_tabla.csv' WITH (FORMAT csv, HEADER, DELIMITER E',', NULL '\N', ENCODING 'UTF-8');
 
-/*
+
 CREATE SCHEMA IF NOT EXISTS pl1;
 SET search_path TO pl1, public;
 CREATE TABLE IF NOT EXISTS pl1.estudiantes2(
         estudiante_id TEXT,
         nombre TEXT,
         codigo_carrera int,
+        edad int,
         indice int
 );
 
 \echo 'Cargando'
 \COPY pl1.estudiantes2    FROM './Datos_tabla.csv' WITH (FORMAT csv, HEADER, DELIMITER E',', NULL '\N', ENCODING 'UTF-8');
-
+/*
 CREATE INDEX id_ASC_indice ON pl1.estudiantes2 (indice ASC NULLS LAST);
-
 */
+
 
 
 COMMIT;
