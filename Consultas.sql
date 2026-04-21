9b387f59dd686376c63f5b440757d4f3fb66da80
AlbaZoydo <alba.zoydo@edu.uah.es>
2026-04-21 09:41:45 +0200

    a

---
+++ Consultas.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+INSERT INTO musicos.Grupos VALUES (1, 'One Republic', 'POP', 'Reino Unido', 'www.');
+
+INSERT INTO musicos.Musicos VALUES (1, '1234567Q','Musico1', 'su casa', 98764, 'Londres', 'provincia', 975675329, 'voz', 1);
+
+COMMIT;
